b5583ad3982914a9d6b8cde3316685d19853dfdc
Ryan Kerluke <146582651+ksfryan@users.noreply.github.com>
2024-11-29 05:03:14 +0000

    test case

--- assignment.py
+++ assignment.py
@@ -1,16 +1,15 @@
+#the variuble a is the users input number
+
 def calculate_pi(iterations):
-    return 4 * sum((-1)**i / (2*i + 1) for i in range(iterations))
+    return 4 * sum((-1)**a / (2*a + 1) for a in range(iterations))
 
 if __name__ == "__main__":
-    
-
     while True:
         a = input(" ")
-        if a.lower() == 'exit':
-            
-            break
-
         
+        if not a.isdigit() or int(a) < 1:
+            print("Error put in a positive number no negatives or decimals")  #This is the test case for positive numbers for this asignment
+            continue
 
         iterations = int(a)
         pi = calculate_pi(iterations)
