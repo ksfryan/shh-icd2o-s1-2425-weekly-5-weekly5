9434af544d12b3bea5b8b66dcbf8128f4ba3b025
Ryan Kerluke <146582651+ksfryan@users.noreply.github.com>
2024-11-29 04:50:00 +0000

    test without ettot test case

--- assignment.py
+++ assignment.py
@@ -1,6 +1,17 @@
-def calculatePi(digits):
-  result = 0
-  return result
+def calculate_pi(iterations):
+    return 4 * sum((-1)**i / (2*i + 1) for i in range(iterations))
 
-number_of_digits = input()
-print(calculatePi(number_of_digits))
+if __name__ == "__main__":
+    
+
+    while True:
+        a = input(" ")
+        if a.lower() == 'exit':
+            
+            break
+
+        
+
+        iterations = int(a)
+        pi = calculate_pi(iterations)
+        print(f"{iterations} iterations:{pi:.10f}")
